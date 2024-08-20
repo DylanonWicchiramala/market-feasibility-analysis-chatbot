a61d5ca176ed7f00e8de954c1135fdb2674c9fb7
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-08-20 12:57:47 +0700

    make tools function return string

--- tools.py
+++ tools.py
@@ -1,12 +1,45 @@
-from gplace import find_place_from_text, find_location, nearby_search
+import gplace
+
+# %%
+def find_place_from_text(location:str):
+    """Finds a place and related data from the query text"""
+    
+    result = gplace.find_place_from_text(location)
+    r = result['candidates'][0]
+    return f"""
+    address: {r['formatted_address']}\n
+    location: {r['geometry']['location']}\n
+    name: {r['name']}\n
+    opening hours: {r['opening_hours']}\n
+    rating: {r['rating']}\n
+    """
+    
+def nearby_search(keyword:str, location:str, radius=2000, place_type=None):
+    """Searches for many places nearby the location based on a keyword. using keyword like \"coffee shop\", \"restaurants\". radius is the range to search from the location"""
+    location = gplace.find_location(location, radius=radius)
+    result = gplace.nearby_search(keyword, location, radius)
+    
+    strout = ""
+    for r in result:
+        strout = strout + f"""
+        address: {r['vicinity']}\n
+        location: {r['geometry']['location']}\n
+        name: {r['name']}\n
+        opening hours: {r['opening_hours']}\n
+        rating: {r['rating']}\n
+        plus code: {r['plus_code']['global_code']}\n\n
+        """
+    return strout
+
+# %%
+# gplace_tools.py
 from langgraph.prebuilt import ToolNode
 from langchain_core.tools import tool
 
 find_place_from_text = tool(find_place_from_text)
-find_location = tool(find_location)
 nearby_search = tool(nearby_search)
 
-tools = [find_place_from_text, find_location, nearby_search]
+tools = [find_place_from_text, nearby_search]
 
 # Create ToolNodes for each tool
 tool_node = ToolNode(tools)
