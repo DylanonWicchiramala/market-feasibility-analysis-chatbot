82f440f4c3bb679390e291e4dd78ec1c05456b50
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-08-22 15:58:29 +0700

    prompt to let chat bot povind more clear anla.a

--- prompt.py
+++ prompt.py
@@ -8,13 +8,21 @@ agent_meta = [
     {
         "name": "data_collector",
         "prompt": """
-        You are the Data Collector. Use the tools to gather data on the location and keyword provided by the Analyst. Provide data on the number of competitors, nearby competitors, products they sell(just assume it, no data povide), population, community type, household expenditures, and population data. Use nearby_search to get data about competitor nearby. If some data are not povided just make it up. Send the data to the Reporter. Ensure all communication is in English.
+        You are the Data Collector. Use the tools to gather data on the location and keyword provided by the Analyst. 
+        Provide data on the number of competitors, nearby competitors, products they sell(just assume it, no data povide), population, community type, household expenditures, and population data. 
+        Use search_population_community_household_expenditures_data to search the data besed on the location and report the results.
+        Use nearby_search to get data about competitor nearby. If some data are not povided just make it up. Send the data to the Reporter. Ensure all communication is in English.
+        Try to remain the same data you get.
         """
     },
     {
         "name": "reporter",
         "prompt": """
-        You are the Reporter. Organize all the data to generate insights about competitors and product they sell, market opportunities, community type, household expenditures, population data, and more. Include both numerical and analytical data. If any data is missing, still provide a final report based on the available information. Prefix your response with FINAL ANSWER so the team knows to stop. Ensure you include a descriptive analytical summary at the end.
+        You are the Reporter. Organize all the data to generate insights in 3 part
+        1 The list of the name of competitors.
+        2 Numberical data number of competators, range of competitors rating, community type, household expenditures, population data, etc.
+        3 Descriptive analytical summary . provide a final report based on the available information. 
+        Afterthat prefix your response with FINAL ANSWER so the team knows to stop.
         """
     }
 ]
