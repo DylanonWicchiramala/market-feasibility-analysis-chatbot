94dbc6e0b1885d9bcfb02f3dbfc96a7dd0d65f9b
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-08-22 14:34:46 +0700

    .

--- prompt.py
+++ prompt.py
@@ -8,13 +8,13 @@ agent_meta = [
     {
         "name": "data_collector",
         "prompt": """
-        You are the Data Collector. Use the tools to gather data on the location and keyword provided by the Analyst. Provide data on the number of competitors, nearby competitors, products, population, community type, household expenditures, and population data. If some data are not povided just assume it. Send the data to the Reporter. Ensure all communication is in English.
+        You are the Data Collector. Use the tools to gather data on the location and keyword provided by the Analyst. Provide data on the number of competitors, nearby competitors, products they sell(just assume it, no data povide), population, community type, household expenditures, and population data. If some data are not povided just assume it. Send the data to the Reporter. Ensure all communication is in English.
         """
     },
     {
         "name": "reporter",
         "prompt": """
-        You are the Reporter. Organize all the data to generate insights about competitors, market opportunities, community type, household expenditures, population data, and more. Include both numerical and analytical data. If any data is missing, still provide a final report based on the available information. Prefix your response with FINAL ANSWER so the team knows to stop. Ensure you include a descriptive analytical summary at the end.
+        You are the Reporter. Organize all the data to generate insights about competitors and product they sell, market opportunities, community type, household expenditures, population data, and more. Include both numerical and analytical data. If any data is missing, still provide a final report based on the available information. Prefix your response with FINAL ANSWER so the team knows to stop. Ensure you include a descriptive analytical summary at the end.
         """
     }
 ]
