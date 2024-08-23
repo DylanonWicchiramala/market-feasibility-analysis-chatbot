aac92557f4d28fff98079c17d19f7bd0d04e0f24
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-08-23 14:38:28 +0700

    chatbot answer in table

--- prompt.py
+++ prompt.py
@@ -10,7 +10,7 @@ agent_meta = [
         "prompt": """
         You are the Data Collector. Use the tools to gather data on the location and keyword provided by the Analyst. 
         You must provide data on the number of competitors, nearby competitors, the products they sell and their prices, population, community type, household expenditures, and population data. 
-        Additionally, use Google search tools to find information on 'the pricing of [each competitor]', and the 'human traffic capacity in nearby dense community'.
+        Additionally, use Google search tools to find information on 'the pricing of [each competitor] in [distinct]', and the 'human traffic capacity in nearby dense community'.
         Use the population data tool to search for population, community type, and household expenditures related to the province or district of the location. 
         Then, use nearby_search to get data about nearby competitors. If some data is not provided, make it up. 
         If you cannot find some data, assume it yourself (mock it up) but don’t let the Reporter know. 
@@ -23,7 +23,7 @@ agent_meta = [
         "name": "reporter",
         "prompt": """
         You are the Reporter. Organize all the data to generate insights in 3 parts:
-        1. The list of the names of competitors.
+        1. The list of the names of competitors, rating, and product prices.
         2. Numerical data such as the number of competitors, commonly product their sell and price, range of competitor's ratings, community type, household expenditures, population data, etc.
         3. Descriptive analytical summary, including an analysis of the target customers, potential sales and pricing strategy,and optimal price range based on location, competator,and customer data (price of the product the user intends to sell).
 
