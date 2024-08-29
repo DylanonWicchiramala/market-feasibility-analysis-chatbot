2acd57e17765238ba415c64917bbce5ef57b8e3a
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-08-29 19:29:11 +0700

    notify when function done

--- utils.py
+++ utils.py
@@ -21,6 +21,38 @@ def append_output(func):
     return wrapper
 
 
+def notify_process(func, sound_effect:str="aurora"):
+    sound_effects = {
+        "blow" : "/System/Library/Sounds/blow.aiff",
+        "glass" : "/System/Library/Sounds/glass.aiff",
+        "hero" : "/System/Library/Sounds/Hero.aiff",
+        "basso" : "/System/Library/Sounds/basso.aiff",
+        "bottle" : "/System/Library/Sounds/bottle.aiff",
+        "frog" : "/System/Library/Sounds/frog.aiff",
+        "funk" : "/System/Library/Sounds/funk.aiff",
+        "morse" : "/System/Library/Sounds/morse.aiff",
+        "ping" : "/System/Library/Sounds/ping.aiff",
+        "pop" : "/System/Library/Sounds/pop.aiff",
+        "purr" : "/System/Library/Sounds/purr.aiff",
+        "sosumi" : "/System/Library/Sounds/sosumi.aiff",
+        "submarine" : "/System/Library/Sounds/submarine.aiff",
+        "tink" : "/System/Library/Sounds/tink.aiff",
+        "aurora" : "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/AlertTones/Modern/Aurora.m4r",
+        "Alert".lower() : "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/AlertTones/Classic/Alert.m4r",
+        "Anticipate".lower() : "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/AlertTones/Classic/Anticipate.m4r",
+        "Apex".lower() : "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/Ringtones/Apex.m4r",
+        "".lower() : "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/AlertTones/Modern/Aurora.m4r",
+    }
+    
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        os.system('afplay '+ sound_effects[sound_effect.lower()])
+        return output
+    
+    return wrapper
+
+
 def load_agent_meta():
     with open('./prompt.json', 'r') as file:
         prompt_data = json.load(file)
