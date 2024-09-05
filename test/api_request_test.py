684b9d2959bcbe6faf846f5b67b32cac1fd801e3
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-09-05 13:38:51 +0700

    count time in api test

--- test/api_request_test.py
+++ test/api_request_test.py
@@ -5,13 +5,15 @@ sys.path.append(dirname(realpath(__file__)) + sep + pardir)
 
 from utils import notify
 import requests
+from time import time
 
 url = "https://market-feasibility-analysis-chatbot-2-jelvbvjqna-uc.a.run.app/test"
 # url = "http://127.0.0.1:8080/test"
 
 headers = {"Content-Type": "application/json"}
-data = {"message": "Hello"}
+data = {"message": "ทำ feasibility report เกี่ยวร้านเกมแถวสยาม"}
 
+stt = time()
 response = requests.post(url, json=data, headers=headers)
 
 if response.status_code == 200:
@@ -20,3 +22,5 @@ if response.status_code == 200:
 else:
     print("Error:", response)
     notify("hero")
+
+print(f"process exetcution time {round(time()-stt,2)} sec.")
